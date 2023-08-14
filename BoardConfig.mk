59883b9ddb5f25d8aaf3283a4e43e463d5f8e60a
LesGaR <43041346+LesGaR@users.noreply.github.com>
2023-08-14 11:36:47 +0200

    Revert "Mpa220v way"
    This reverts commit fbbc9654e22b0c2a91adbf4cbb161a9e06b6050c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,22 +66,21 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-#Use abl cmdline
-#BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
-#BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
-#BOARD_KERNEL_CMDLINE += androidboot.memcg=1
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-#BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
-#BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
-#BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
-#BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0x880000
-#BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
-#BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
-#BOARD_KERNEL_CMDLINE += pcie_ports=compat
-#BOARD_KERNEL_CMDLINE += service_locator.enable=1
-#BOARD_KERNEL_CMDLINE += swiotlb=0
-#BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
-#BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
+BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0x880000
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += pcie_ports=compat
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += swiotlb=0
+BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1d84000.ufshc
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := nabu_user_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/nabu
